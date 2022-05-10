6274e91525bc0b4b41ab5d3719fbda9401490cdd
Ethan <esadowski1@my.bcit.ca>
2022-05-09 17:09:20 -0700

    status/script transfer implemented

--- src/scripts/server/assign_db.sh
+++ src/scripts/server/assign_db.sh
@@ -10,14 +10,24 @@ echo $IOC_TYPE
 #Get the IP address of  PV $2
 echo "Determining IOC IP Address..."
 IP=$(echo $(cainfo $TARGET_IOC:device) | grep Host: | grep -Pom 1 '[0-9.]{7,15}')
-echo "Configuring IOC at IP: $IP"
 
+if [ ! -z $IP ]
+then 
+
+echo "Configuring IOC at IP: $IP"
 echo "Pushing config to IOC"
 #Copy the corresponding IOC db to a generic named file 'dbconfig.db'
 cp ../database/IOC/${IOC_TYPE} ../database/dbconfig.db
+cp ../scripts/IOC/${IOC_TYPE} ../scripts/run_script.py
 
 #Opens a secure transfer ssh connection to transfer the dbconfig.db file
-sshpass -p "esado" scp ../database/dbconfig.db esado@${IP}:~
+sshpass -p "triumf" scp ../database/dbconfig.db pi@${IP}:~
+sshpass -p "triumf" scp ../scripts/run_script.py pi@${IP}:~
 
 #Opens a ssh connection to kill the current IOC, change the IOC_ID on the IOC device and re-run the IOC
-sshpass -p "esado" ssh -t esado@${IP} "pkill screen; sed -i 's/ID=${TARGET_IOC}/ID=${IOC_ID_UPDATE}/' ~/env/IOC_CONFIG && screen -d -m sh -c '/opt/epics/epics-base/bin/linux-x86_64/softIoc -m IOC=${IOC_ID_UPDATE} -d dbconfig.db; exec bash'"
+sshpass -p "triumf" ssh -t pi@${IP} "pkill screen; sed -i 's/ID=${TARGET_IOC}/ID=${IOC_ID_UPDATE}/' ~/env/IOC_CONFIG && screen -d -m sh -c '~/EPICS/epics-base/bin/linux-arm/softIoc -m IOC=${IOC_ID_UPDATE} -d dbconfig.db; python ~/run_script.py; exec bash'"
+
+else
+	echo "RPI with that PV not available!"
+fi
+
