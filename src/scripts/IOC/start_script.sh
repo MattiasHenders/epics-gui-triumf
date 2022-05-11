fc633a246ec5553940ce99b3512f1ffd56a36141
Ethan <esadowski1@my.bcit.ca>
2022-05-11 16:43:51 -0700

    db file updates, fixed ssh issue, finalized ioc start scripting

--- src/scripts/IOC/start_script.sh
+++ src/scripts/IOC/start_script.sh
@@ -2,36 +2,36 @@
 
 source ./env/IOC_CONFIG
 
+echo "Attempting IOC Startup with ID: ${ID}"
+
 PV_LIST=./env/PV_LIST
 PVS=`cat $PV_LIST`
-
-echo "Attempting IOC Startup with ID: ${ID}"
 
 caget "${ID}:status.INAV"
 status=$(caget "${ID}:status.INAV")
 status_sub=${status: -2}
 
+
 if [ $status_sub == "NC" ]
 then
-	echo "Starting IOC with ID: ${ID}"
-	screen -d -m softIoc -m IOC=${ID} -d dbconfig.db
+        echo "Starting IOC with ID: ${ID}"
+        screen -d -m sh -c "python ~/run_script.py && softIoc -m IOC=${ID} -d dbconfig.db; exec bash"
 else
-	for IOC_PV in $PVS
-	do
-		if [ ! $IOC_PV == $ID ]
-		then
-			status=$(caget "${IOC_PV}:status.INAV")
-			status_sub=${status: -2}
-			if [ $status_sub == "NC" ]
-			then
-				sed -i 's/ID=${ID}/ID=${$IOC_PV}/' ~/env/IOC_CONFIG
-				echo "Starting deviceless IOC with ID: ${ID}"
-				screen -d softIoc -m IOC=${ID} -d ND_IOC.db
-			fi
-		fi
-	done
+        for IOC_PV in $PVS
+        do
+                if [ ! $IOC_PV == $ID ]
+                then
+                        status=$(caget "${IOC_PV}:status.INAV")
+                        status_sub=${status: -2}
+                        if [ $status_sub == "NC" ]
+                        then
+                                echo $IOC_PV
+                                sed -i 's/ID=${ID}/ID=${IOC_PV}/' ~/env/IOC_CONFIG
+                                echo "Starting deviceless IOC with ID: ${IOC_PV}"
+                                echo ${IOC_PV}
+                                screen -d -m sh -c "python ~/run_script.py && softIoc -m IOC=${IOC_PV} -d ND_IOC.db; exec bash"
+				break
+                        fi
+                fi
+        done
 fi
-
-
-
-
