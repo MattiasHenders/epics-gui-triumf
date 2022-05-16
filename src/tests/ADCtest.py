f05255db53a48c3b4c6e5130347e6a17596617bd
buhryan <bxing5@gmail.com>
2022-05-16 13:19:26 -0700

    added ADC test script

---
+++ src/tests/ADCtest.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+from epics import PV
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan = AnalogIn(mcp, MCP.P0)
+
+voltagePV = PV('voltage')
+while True:
+	print("Raw ADC Value: ", chan.value)
+	print("ADC Voltage: " + str(chan.voltage) + "V")
+    volpagePV.put(chan.voltage)
+	time.sleep(0.5)
