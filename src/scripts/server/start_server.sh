12e8d6f4acd4f25fbfab7233c2c47f8353aa1ac3
Ethan Sadowski <esadowski1@my.bcit.ca>
2022-05-19 10:18:29 -0700

    updating

--- src/scripts/server/start_server.sh
+++ src/scripts/server/start_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-softIoc -d ../../database/server/iocstatus.db
+echo "STARTING EPICS SERVER"
+## Add HLA startup here ##
+screen -d -m sh -c 'softIoc -d ../../database/server/iocstatus.db; exit'
+
