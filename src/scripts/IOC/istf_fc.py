213858900ada154bcd78dc4402f8f0ac41c1703a
MattiasHenders <henders.mattias@gmail.com>
2022-05-20 12:27:32 -0700

    Range added to loops

--- src/scripts/IOC/istf_fc.py
+++ src/scripts/IOC/istf_fc.py
@@ -89,10 +89,10 @@ def setup():
 
     GPIO.setmode(GPIO.BCM)
 
-    for i in gpioList:
-        GPIO.setup(i, (GPIO.IN, GPIO.OUT)[gpioOutputList[i]])
+    for i in range(len(gpioList)):
+        GPIO.setup(gpioList[i], (GPIO.IN, GPIO.OUT)[gpioOutputList[i]])
         if gpioOutputList[i]:
-            GPIO.output(i, GPIO.HIGH)
+            GPIO.output(gpioList[i], GPIO.HIGH)
         previousList.append(False)
     ####################################################
     # SET the interlock devices and interlocks
