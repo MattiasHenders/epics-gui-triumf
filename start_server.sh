8a05c611d755884b84f1edaa2a886c7f667292f9
Ethan Sadowski <esadowski1@my.bcit.ca>
2022-05-23 14:28:44 -0700

    added comments

--- start_server.sh
+++ start_server.sh
@@ -2,6 +2,8 @@
 
 echo "STARTING EPICS SERVER"
 ## Add HLA startup here ##
+pkill screen
 screen -d -m sh -c 'cd high-level-application && flask run --host 0.0.0.0'
 screen -d -m sh -c 'softIoc -d src/database/server/iocstatus.db; exit'
-
+screen -list
+echo "SERVER STARTED"
